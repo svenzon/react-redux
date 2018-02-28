e5becaf17a2c11f2346772d16ed7c2cc5a7134fa
svenzon <anders.m.sv@gmail.com>
2018-02-28 13:11:40 +0100

    Mock APIs and course loader implemented successfully

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import { render } from "react-dom";
 import configureStore from "./store/configureStore";
 import {Provider} from "react-redux";
 import Routes from "./routes";
+import {loadCourses} from "./actions/courseActions";
 import App from "./components/App";
 import "./styles/styles.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
     <Provider store={store}>
