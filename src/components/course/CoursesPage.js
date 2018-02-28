d79e980d4680bb2e09348fd6f406c738a0f4661b
svenzon <anders.m.sv@gmail.com>
2018-02-28 15:03:07 +0100

    Hunting for #130 error

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,23 +1,20 @@
 import React from "react";
 import { PropTypes } from "prop-types";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as courseActions from "../../actions/courseActions";
+import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
     }
 
-    courseRow(course, index) {
-        return <div key={index}>{course.title}</div>;
-    }
-
     render() {
         return (
             <div>
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
+                <CourseList courses={this.props.courses}/>
             </div>
         );
     }
