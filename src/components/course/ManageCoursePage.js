eae94c57542c241932620468a553634dabc9741f
svenzon <anders.m.sv@gmail.com>
2018-03-01 14:49:55 +0100

    Inputs added, coursePage updated

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,15 +3,24 @@ import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import PropTypes from "prop-types";
 import * as courseActions from "../../actions/courseActions";
+import CourseForm from "./courseForm";
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
         super(props, context);
+
+        this.state = {
+            course: Object.assign({}, this.props.course),
+            errors: {}
+        }
     }
 
     render() {
         return (
-            <h1>Manage Course</h1>
+            <div>
+                <h1>Manage Course</h1>
+                <CourseForm course={this.state.course}/>
+            </div>
         );
     }
 }
@@ -21,6 +30,7 @@ ManageCoursePage.PropTypes = {
 };
 
 function mapStateToProps(state, ownProps) {
+    let course = {id: "", watchHref: "", title: "", authorId: "", length: "", category: ""};
     return {
         state: state
     };
