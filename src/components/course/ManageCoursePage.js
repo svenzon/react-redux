ef76bfbb2e340c5b0a161bb648775a8ebbe743ec
svenzon <anders.m.sv@gmail.com>
2018-03-01 15:54:46 +0100

    New actiontypes Course manager updated InitialState corrected

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -13,12 +13,22 @@ class ManageCoursePage extends React.Component {
             course: Object.assign({}, this.props.course),
             errors: {}
         };
+
+        this.updateCourseState = this.updateCourseState.bind(this);
+    }
+
+    updateCourseState(event) {
+        const field = event.target.name;
+        let course = Object.assign({}, this.state.course);
+        course[field] = event.target.value;
+        return this.setState({course: course});
     }
 
     render() {
         return (
             <CourseForm
-                allAuthors={[]}
+                allAuthors={this.props.authors}
+                onChange={this.updateCourseState}
                 course={this.state.course}
                 errors={this.state.errors}
             />
@@ -26,14 +36,24 @@ class ManageCoursePage extends React.Component {
     }
 }
 
-ManageCoursePage.PropTypes = {
-    course: PropTypes.object.isRequired
+ManageCoursePage.propTypes = {
+    course: PropTypes.object.isRequired,
+    authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
     let course = {id: "", watchHref: "", title: "", authorId: "", length: "", category: ""};
+
+    const authorsFormattedForDropdown = state.authors.map(author => {
+        return {
+            value: author.id,
+            text: author.firstName + " " + author.lastName
+        };
+    });
+
     return {
-        course: course
+        course: course,
+        authors: authorsFormattedForDropdown
     };
 }
 
